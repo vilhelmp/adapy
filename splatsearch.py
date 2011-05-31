92fb93778df078ec911f5b0fa46b3ffd5367116f
Magnus Vilhelm Persson <magnusp@mintaka>
2011-05-31 11:53:38 +0200

    splatsearch.py finished

--- splatsearch.py
+++ splatsearch.py
@@ -11,11 +11,11 @@ This script queries Splatalogue.net
 
 Returns a data structure with the requested information from Splatalogue.
 
-
-
 TODO : if there are more than 1 result page, get it to read them all \
         in to the data structure. 
 TODO : page output if it is long
+
+TODO : solve the parsing, the removal of tags...
 
 """
 
@@ -24,6 +24,34 @@ TODO : page output if it is long
 # import optparser
 from urllib2 import urlopen
 from ClientForm import ParseResponse
+from BeautifulSoup import BeautifulSoup as bfs
+from scipy import array, where, nan, arange
+
+#import os
+#import sys
+from optparse import OptionParser as op
+#import pyfits as pf
+#from string import lower
+
+#version
+ver = 1.0
+
+desc="""Search the splatalogue compilation"""
+usage = "usage: %prog [options]"
+epilog = """usage: %prog [options]"""
+
+parser = op(usage=usage,description=desc,epilog=epilog, version="%prog " + str(ver))
+
+# the options permitted
+parser.add_option("-f", "--frequency", dest="f", help="frequency range given as f1,f2" , metavar="FREQUENCY")
+#parser.add_option("-v", "--velocities", dest="v", help="between what velocities to plot", metavar="VELOCITIES")
+#parser.add_option("-b", "--box", dest="b", help="zoom to a box X,Y size", metavar="BOX")
+#parser.add_option("-l", "--list", action="store_true", dest="l", help="list the header", metavar="LIST")
+#parser.add_option("-k", "--force", action="store_true", dest="k", help="force overwriting of keyword", metavar="FORCE")
+
+# time to parse
+(options, args) = parser.parse_args()
+
 
 # get the form from the splatalogue search page
 response = urlopen("http://www.cv.nrao.edu/php/splat/b.php")
@@ -32,8 +60,19 @@ response.close()
 form = forms[0]
 
 #now change the 'from' and 'to' controls with the frequency range requested
-form['from'] = str(203.406)
-form['to'] = str(203.408)
+# if no filename is supplied
+if options.f == None:
+    print 'no options input, running example with f1,f2 = 203.406,203.408'
+    parser.print_usage()
+    form['from'] = str(203.406)
+    form['to'] = str(203.408)
+else:
+    # the options
+    f1,f2 = [x for x in (options.f).split(',')]
+    form['from'] = f1
+    form['to'] = f2
+
+
 form['frequency_units'] = ['GHz']
 
 # 'click' the form
@@ -43,132 +82,62 @@ form['frequency_units'] = ['GHz']
 result = urlopen(form.click_request_data()[0]+'&submit=Search').read() # this works
 # parse the results
 
-####### Alternative 1
-from lxml import html
-
-# breaks the <br> tag too, have to change the xpath string input parameter
-table = html.fromstring(result)
-# get everything directly
-# change the xpath parameters. look at 
-# http://www.stonehenge.com/merlyn/LinuxMag/col92.html for reference
-findTR = '//table[@class="results"]/tr'
-findTD = './td[position()=1]/a/@href | ./td[position()>1]/text() | self::node()[position()=1]/td/text()'
-rows = [row.xpath(findTD) for row in table.xpath(findTR)]
-for row in rows:
-    print len(row)*'%7s\t' % tuple(row)
-for row in table.xpath(findTR):
-    column = row.xpath(findTD)
-    l = len(column)
-    print l*'%7s\t' % tuple(column)
-
-
-####### Alternative 2
-# works well, code is really short
-# need to remove <br> and &nbsp; <a href etc tags
-# since &nbsp: means no data, replace with ND, Nan or similar
-# inspired by http://blog.jgc.org/2009/11/parsing-html-in-python-with.html
-from BeautifulSoup import BeautifulSoup as bfs
-
-#bfs.NESTABLE_TAGS['a'] = ['td']
-#bfs.NESTABLE_TAGS['sub'] = ['td']
+bfs.NESTABLE_TABLE_TAGS['td'].append('sub')
+bfs.NESTABLE_TABLE_TAGS['td'].append('sup')
+bfs.NESTABLE_TABLE_TAGS['td'].append('font')
 
 soup = bfs(result)
 
 
-def findreplace(soup, x):
-    r = soup.findAll(x)
-    [i.replaceWith(i.renderContents()) for i in r]
+def findreplace(isoup, x,p=0):
+    r = isoup.findAll(x)
+    if p:
+        [i.replaceWith('('+i.renderContents()+')') for i in r]
+    else:
+        [i.replaceWith(i.renderContents()) for i in r]
 
-# these work
+# these work, but only in this order (!?)
+findreplace(soup, 'font')
+findreplace(soup, 'sub')
+findreplace(soup, 'sup',p=1)
 findreplace(soup, 'a')
 findreplace(soup, 'br')
 
-# these does not work!!!!!!!!!!!!!!!!
-findreplace(soup, 'sub')
-findreplace(soup, 'sup')
-
-
-
 results_table = soup.find('table',"results")
-
-for r in results_table.findAll(True):
-    if r not in ['table', 'tr', 'td']:
-        r.replaceWith(r.renderContents())
-
 
 data = [[col.renderContents() for col in row.findAll('td')] for row in results_table.findAll('tr')]
 
-# very ugly solution to remove sub, sup, font and a href tags from
-# table cells
-for line in data:
-    for i in line:
-        s = bfs(i)        
-        for tag in s.findAll(True):
-            for i, x in enumerate(tag.parent.contents):
-                if x == tag: 
-                    break
-                else:
-                    print "Can't find", tag, "in", tag.parent
-                    continue
-            for r in reversed(tag.contents):
-              tag.parent.insert(i, r)
-            tag.extract()
+# put all the stuff in arrays
+data[0][0] = 'N'
+col_names = data[0]
+data = data[1:]
 
-## another try
-for line in data:
-    for i in line:  
-        for tag in s.findAll('a'):
-            print s
-            tag.replaceWith(tag.renderContents())
-            print s
+N, chem, name, cfreq, mfreq, qns,cdmsjpl_I,lovas_I,EL,llist = array(data).transpose()
 
 
-####### Alternative 3
-# need file in parse cmd, I have result string
-# does not work
-from xml.etree.ElementTree import ElementTree
+## this is a not so good coded part...
+# running i.strip('(').strip(')').split()
+# does not seemt to work either
+cfreq[where(cfreq == '&nbsp;')] = 'nan nan'
+mfreq[where(mfreq == '&nbsp;')] = 'nan nan'
 
-doc = ElementTree().parse(result)
+cfreq = array([i.split(' ') for i in cfreq])
+mfreq = array([i.split(' ') for i in mfreq])
 
-for t in doc.findall('.//table'):
-    for tr in t.findall('./tr/')[1:]: # skip the header row
-    tds = tr.findall('./td')
-    print tds[0][0].attrib['href'], tds[1].text.strip(), tds[2].text.strip()
-
-
-
-
-## here I am cheating because the above did not work properly 
-# what I am doing instead is just taking the search string, 
-# and manipulating it, the control "submit=Search" is not included in the "click"
+cfreqerr = cfreq[:,1]
+mfreqerr = mfreq[:,1]
+cfreq = cfreq[:,0]
+mfreq = mfreq[:,0]
 
 
-#~ f = ['http://www.cv.nrao.edu/php/splat/c.php?calcIn=',
- #~ 'data_version=v2.0',
- #~ 'from=203.406',
- #~ 'to=203.408',
- #~ 'frequency_units=GHz',
- #~ 'energy_range_from=',
- #~ 'energy_range_to=',
- #~ 'lill=on',
- #~ 'tran=',
- #~ 'submit=Search',
- #~ 'no_atmospheric=no_atmospheric',
- #~ 'no_potential=no_potential',
- #~ 'no_probable=no_probable',
- #~ 'displayLovas=displayLovas',
- #~ 'displaySLAIM=displaySLAIM',
- #~ 'displayJPL=displayJPL',
- #~ 'displayCDMS=displayCDMS',
- #~ 'displayToyaMA=displayToyaMA',
- #~ 'displayOSU=displayOSU',
- #~ 'displayRecomb=displayRecomb',
- #~ 'displayLisa=displayLisa',
- #~ 'displayRFI=displayRFI',
- #~ 'ls1=ls1',
- #~ 'ls5=ls5',
- #~ 'el1=el1']
+mfreqerr = [i.strip('(').strip(')') for i in  mfreqerr]
+cfreqerr = [i.strip('(').strip(')') for i in  cfreqerr]
 
-#~ f = '&'.join(f)
-#~ result = urlopen(f).read() # this works
+cfreqerr = array(cfreqerr, dtype='float')
+cfreq = array(cfreq, dtype='float')
+mfreqerr = array(mfreqerr, dtype='float')
+mfreq = array(mfreq, dtype='float')
 
+print '{0:2} {1:10}\t{2:10}\t{3:9}\t{4:6} '.format('N', 'Form', 'Freq', 'FErr', 'List')
+for i in arange(len(N)):
+    print '{0:2} {1:10}\t{2:10}\t{3:9}\t{4:6} '.format(N[i], chem[i], cfreq[i], cfreqerr[i], llist[i])
